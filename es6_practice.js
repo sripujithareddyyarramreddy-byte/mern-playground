effad52185ce6d629f0418d19ec4f241a02a6e5b
sripujithareddyyarramreddy-byte <sripujithareddyyarramreddy@gmail.com>
2026-06-04 23:29:20 +0530

    First MERN setup

---
+++ es6_practice.js
@@ -0,0 +1,16 @@
+// Arrow function
+const add = (a, b) => a + b;
+
+console.log('Sum:', add(3, 7));
+
+// Template literal
+const name = 'MERN Student';
+
+console.log(`Hello, ${name}!`);
+
+// Spread operator
+const nums = [1, 2, 3];
+
+const moreNums = [...nums, 4, 5];
+
+console.log(moreNums);
